80d2aa6ac006dadc285dfcefd27619a3dffc349f
schitte <schitteindustries@gmail.com>
2017-09-22 22:51:26 -0400

    Update moduleNav.js

--- module/moduleNav.js
+++ module/moduleNav.js
@@ -1,13 +1,15 @@
 var moduleNav = {
-  view: return m("div.schitte-nav", [
-    m("img.schitte-nav-logo"),
-    m("span.schitte-nav-title"),
-    m("div.schitte-nav-buttons", [
-      m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/home", "_blank")']", "Home"),
-      m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/schitte-products", "_blank")']", "Products"),
-      m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/clash-of-cash", "_blank")']", "Clash of Cash"),
-      m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/sellout", "_blank")']", "Sellout"),
-      m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/disclaimer", "_blank")']", "Disclaimer"),
-    ])
-  ]);
+  view: function() {
+    return m("div.schitte-nav", [
+      m("img.schitte-nav-logo"),
+      m("span.schitte-nav-title"),
+      m("div.schitte-nav-buttons", [
+        m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/home", "_blank")']", "Home"),
+        m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/schitte-products", "_blank")']", "Products"),
+        m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/clash-of-cash", "_blank")']", "Clash of Cash"),
+        m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/sellout", "_blank")']", "Sellout"),
+        m("button.schitte-nav-button[onclick='window.open("https://sites.google.com/view/schitteindustries/disclaimer", "_blank")']", "Disclaimer"),
+      ])
+    ]);
+  }
 }
