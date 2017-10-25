46b98531eae7334581e80bdadad395229e68a36b
Schitte Industries <schitteindustries@gmail.com>
2017-10-25 16:54:46 -0400

    Update moduleNav.js

--- module/moduleNav.js
+++ module/moduleNav.js
@@ -4,8 +4,8 @@ var moduleNav = {
       m("img.schitte-nav-logo"),
       m("span.schitte-nav-title"),
       m("div.schitte-nav-buttons", [
-        m("button.schitte-nav-button[onclick='window.open(\"https://sites.google.com/view/schitteindustries/home\", \"_blank\")']", "Home"),
-        m("button.schitte-nav-button[onclick='window.open(\"https://sites.google.com/view/schitteindustries/schitte-products\", \"_blank\")']", "Products"),
+        m("button.schitte-nav-button[onclick='window.open(\"https://sites.google.com/view/schitteindustries\", \"_blank\")']", "Home"),
+        m("button.schitte-nav-button[onclick='window.open(\"https://sites.google.com/view/schitteindustries/about\", \"_blank\")']", "About"),
         m("button.schitte-nav-button[onclick='window.open(\"https://sites.google.com/view/schitteindustries/clash-of-cash\", \"_blank\")']", "Clash of Cash"),
         m("button.schitte-nav-button[onclick='window.open(\"https://sites.google.com/view/schitteindustries/sellout\", \"_blank\")']", "Sellout"),
         m("button.schitte-nav-button[onclick='window.open(\"https://sites.google.com/view/schitteindustries/disclaimer\", \"_blank\")']", "Disclaimer"),
